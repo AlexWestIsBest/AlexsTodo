060e87469fee9757576b6352d3c0242dea6d63c9
Alex Westerlund <alexwesterlund@outlook.com>
2024-01-30 20:49:06 -0500

    Initial Commit

---
+++ Routine2/Routine2App.swift
@@ -0,0 +1,17 @@
+//
+//  Routine2App.swift
+//  Routine2
+//
+//  Created by Alex Westerlund on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Routine2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
