dcc7325b1f004d31bb1d74d0a3e36720209e2b65
Alex Westerlund <alexwesterlund@outlook.com>
2024-04-22 18:01:51 -0400

    Creates blank home page view for MVP

---
+++ Routine2/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  Routine2
+//
+//  Created by Alex Westerlund on 4/22/24.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
